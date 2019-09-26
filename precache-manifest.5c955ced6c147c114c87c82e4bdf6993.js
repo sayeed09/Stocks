e157d03adb9316f8a1187b2cda33402eb94b36d9
sayeed09 <sayeedshaikh09@gmail.com>
2019-09-26 22:32:14 +0530

    Updates

---
+++ precache-manifest.5c955ced6c147c114c87c82e4bdf6993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d4bbd1bab6d5e15dce1a9b1b61352e",
+    "url": "/Stocks/index.html"
+  },
+  {
+    "revision": "c23fbc796d2c5df7fa04",
+    "url": "/Stocks/static/css/main.fcc50c22.chunk.css"
+  },
+  {
+    "revision": "7b5b0dc36399d95492a6",
+    "url": "/Stocks/static/js/2.cb5932c4.chunk.js"
+  },
+  {
+    "revision": "c23fbc796d2c5df7fa04",
+    "url": "/Stocks/static/js/main.c366126d.chunk.js"
+  },
+  {
+    "revision": "0003b41eee797fce1c19",
+    "url": "/Stocks/static/js/runtime~main.54513dd1.js"
+  }
+]);
